22113f15b9ec022b5887d0f2e4db4f47fef8e171
culi <culitif@tuta.io>
2021-08-05 16:39:00 -0700

    load quote component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Quote from '../quote/Quote';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <div className="App">
+    <Quote/>
+  </div>;
 }
